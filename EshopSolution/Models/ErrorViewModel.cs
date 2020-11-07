5679129c1c0388bfaceecf77b747926adc19875a
taingkhac <43800589+taingkhac@users.noreply.github.com>
2020-11-07 09:15:56 +0700

    Add project files.

---
+++ EshopSolution/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EshopSolution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
